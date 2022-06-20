2e55b86bb35b3a58f058a7941eab9cb0c8becbe6
Maykon Sousa <maykon.sousa@hotmail.com>
2022-06-20 01:55:01 -0300

    :sparkles: feat: implementa fluxo de criação de posts

---
+++ src/modules/posts/useCases/CreatePost/CreatePostController.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import { CreatePostUseCase } from './CreatePostUseCase';
+
+export class CreatePostController {
+  async handle(req: Request, res: Response): Promise<Response> {
+    const { yt_url, title, description, is_private } = req.body;
+    const { id: user_id } = req.user;
+    const createPostUseCase = container.resolve(CreatePostUseCase);
+    try {
+      const post = await createPostUseCase.execute({
+        user_id,
+        yt_url,
+        title,
+        description,
+        is_private,
+      });
+      return res.status(201).json(post);
+    } catch (error) {
+      if (error instanceof Error) {
+        return res.status(400).json({
+          error: error.message,
+        });
+      }
+      return res.status(500).json({ error: 'Unexpected error' });
+    }
+  }
+}
--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { authenticateRoutes } from './athenticate.routes';
 import { passwordRoutes } from './password.routes';
+import { postsRoutes } from './posts.routes';
 import { usersRoutes } from './users.routes';
 
 export const router = Router();
 router.use('/users', usersRoutes);
 router.use('/auth', authenticateRoutes);
 router.use('/password', passwordRoutes);
+router.use('/posts', postsRoutes);
