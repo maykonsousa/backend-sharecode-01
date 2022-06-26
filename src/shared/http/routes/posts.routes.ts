9fdfc620e3390f1779c6f10445462904edbce85b
Maykon Sousa <maykon.sousa@hotmail.com>
2022-06-26 06:57:30 -0300

    :bug: fix: ajusta path da rota

--- src/shared/http/routes/posts.routes.ts
+++ src/shared/http/routes/posts.routes.ts
@@ -14,6 +14,6 @@ const findPostsByUserController = new FindPostsByUserController();
 
 postsRoutes.use(EnsureAuthenticated);
 postsRoutes.post('/', createPostController.handle);
-postsRoutes.get('/teste', findPostsByUserController.handle);
+postsRoutes.get('/user/:user_id', findPostsByUserController.handle);
 postsRoutes.get('/:id', findPostByIdController.handle);
 postsRoutes.get('/video/:video_id', findPostByVideoIdController.handle);
